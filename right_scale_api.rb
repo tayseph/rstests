b04fe860c56143a7cb42cc07c211b17544d357be
Tay Carpenter <thc@distraction.(none)>
2010-06-18 18:31:43 -0700

    many changes including the "objection" oriented solution for running a rightscript on a live server.

--- right_scale_api.rb
+++ right_scale_api.rb
@@ -76,7 +76,6 @@ def extricateArrayInfo(ip)
 	#puts "ip.class is " + ip.class.to_s
 	#puts "ip.length is " + ip.length.to_s
 
-
 	i = 0
 	ip.each do |bob|
 		addy, name = bob.split('###')
@@ -107,19 +106,39 @@ def extricateArrayInfo(ip)
 
 end
 
-def extricateArrayIP(myarrays)
+def checkREST(my_instances, instance_type)
+	my_instances[instance_type].each do |server|
+		server.each_key do |s|
+			puts server[s].to_s + "  " + s.to_s	
+		end
+	end
+#	my_instances.each_key do |key|
+#		puts key
+#		puts key.class
+#		puts key.length
+#	end
+
+
+end
+
+def extricateArrayIP(my_instances, instance_type, nickname)
+#	puts my_instances, instance_type, nickname
 	# pulling out a specific URL based on "nickname"
 
+	target_server = []
 	array_nicknames = []
 	ip_list = []
-	myarrays["server_arrays"].each do |server|
+	my_instances[instance_type].each do |server|
 		server.each_key do |s|
-			if server["nickname"] =~ /production/
+			if server["nickname"] =~ /#{nickname}/
 				ip_list << server["href"].to_s+"###"+server["nickname"].to_s
+				#puts server["server_template_href"]
+				#puts server["nickname"]
+				target_server = server
 			end
 		end
 	end 
-	return ip_list.uniq!
+	return ip_list.uniq!, target_server
 end
 
 args = ARGV
@@ -127,22 +146,46 @@ args = ARGV
 #puts args[0]
 #puts args[1]
 
-begin
+#begin
 	creds = init(args)
 	RightScaleAPI::Client.login creds["username"], creds["password"] 
-	#myservers = RightScaleAPI::Client.get('https://my.rightscale.com/api/acct/25875/servers')
-	myarrays = RightScaleAPI::Client.get('https://my.rightscale.com/api/acct/25875/server_arrays')
-	#mydeployments = RightScaleAPI::Client.get('https://my.rightscale.com/api/acct/25875/deployments')
+	#types = [ "right_scripts", "servers", "deployments", "server_arrays" ]
 
-	#showActiveServers(myservers)
-	ip_list = extricateArrayIP(myarrays)
-	extricateArrayInfo(ip_list)
+		#my_instances = RightScaleAPI::Client.get("https://my.rightscale.com/api/acct/25875/#{type}")
+		my_deployments = RightScaleAPI::Client.get('https://my.rightscale.com/api/acct/25875/deployments')
+		my_servers = RightScaleAPI::Client.get('https://my.rightscale.com/api/acct/25875/servers')
+		my_server_arrays = RightScaleAPI::Client.get('https://my.rightscale.com/api/acct/25875/server_arrays')
+		my_right_scripts = RightScaleAPI::Client.get('https://my.rightscale.com/api/acct/25875/right_scripts')
 
-		
+		#instance_type = "right_scripts"
+		instance_type = "server_arrays"
+		nickname = "production-php"
+		#ip_list, server_array = extricateArrayIP(my_server_arrays, instance_type, nickname)
+		#extricateArrayInfo(ip_list)
+
+		#puts server_array.class
+		#puts server_array.length
+
+		params = { "server_array[right_script_href]" => "https://my.rightscale.com/api/acct/25875/right_scripts/226724",  "server_array[server_template_hrefs]" => "https://my.rightscale.com/api/acct/25875/ec2_server_templates/58528"}
+		#server_array["right_script_href"] = "https://my.rightscale.com/api/acct/25875/right_scripts/226724"
+		#server_array["server_template_hrefs"] = "https://my.rightscale.com/api/acct/25875/ec2_server_templates/58528"
+		#puts RightScaleAPI::Client.post('https://my.rightscale.com/api/acct/25875/server_arrays/6747/run_script_on_all.xml', "server_array[right_script_href]=https://my.rightscale.com/api/acct/25875/right_scripts/226724", "server_array[server_template_hrefs]=https://my.rightscale.com/api/acct/25875/ec2_server_templates/58528")
+		#puts RightScaleAPI::Client.post('https://my.rightscale.com/api/acct/25875/server_arrays/6747/run_script_on_all.xml', params )
+		#puts RightScaleAPI::Client.post('/api/acct/25875/server_arrays/6747/run_script_on_all', server_array )
 
 	#start_stop(myservers, "tomcat_test2", args[1])
-rescue => err
-	puts " -- " + err.backtrace.to_s
-	puts "failed do to previous errors"
-end
+#rescue => err
+#	puts " -- " + err.backtrace.to_s
+#	puts "failed do to previous errors"
+#end
+
+ARRAY="https://my.rightscale.com/api/acct/25875/server_arrays/6747/run_script_on_all.xml"
+TEMPLATE="https://my.rightscale.com/api/acct/25875/ec2_server_templates/58528"
+RIGHTSCRIPT="https://my.rightscale.com/api/acct/25875/right_scripts/226724"
+
+
+puts `curl -c youveGotToBeFuckingKiddingMe -u #{creds["username"]}:#{creds["password"]}  https://my.rightscale.com/api/acct/25875/login?api_version=1.0`
+puts `curl -H 'X-API-VERSION: 1.0' -b youveGotToBeFuckingKiddingMe #{ARRAY} -d server_array[right_script_href]=#{RIGHTSCRIPT} -d server_array[server_template_hrefs]=#{TEMPLATE}`
+
+#`curl -d right_script=https://my.rightscale.com/api/acct/25875/right_scripts/226724 -H 'X-API-VERSION: 1.0' -b youveGotToBeFuckingKiddingMe https://my.rightscale.com/api/acct/25875/server_arrays/6747/run_script_on_all`
 
